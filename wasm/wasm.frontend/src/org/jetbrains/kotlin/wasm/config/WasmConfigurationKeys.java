1d6b9c8a308b02dee2c730281acb85ffb466500b
Jim M. R. Teichgräber <jim.teichgraeber@jetbrains.com>
2025-12-18 11:31:11 +0100

    [Wasm] Add new compiler argument `Xwasm-internal-local-variable-prefix` (KT-83162)
    Allows configuring which prefix the Wasm backend prepends to internal
    local variables

--- wasm/wasm.frontend/src/org/jetbrains/kotlin/wasm/config/WasmConfigurationKeys.java
+++ wasm/wasm.frontend/src/org/jetbrains/kotlin/wasm/config/WasmConfigurationKeys.java
@@ -48,6 +48,9 @@ public class WasmConfigurationKeys {
     public static final CompilerConfigurationKey<String> WASM_DEPENDENCY_RESOLUTION_MAP =
             CompilerConfigurationKey.create("provide alternative paths to imported dependency modules.");
 
+    public static final CompilerConfigurationKey<String> WASM_INTERNAL_LOCAL_VARIABLE_PREFIX =
+            CompilerConfigurationKey.create("internal local variable prefix");
+
     public static final CompilerConfigurationKey<Boolean> WASM_COMMAND_MODULE =
             CompilerConfigurationKey.create("use command module initialization (_initialize export).");
 }
