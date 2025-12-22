cb5085bc1ea8e629813a6e91ecdfaafdb8bd1168
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-12-22 14:57:15 +0200

    [Compiler] Unbind `IrGenerationExtension` from intellij extensions API
    Separate project extension point registration is created in AA modules
    for keeping the compatibility in the compiler facility components.
    
    ^KT-83341

--- analysis/analysis-api-impl-base/testFixtures/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/components/compilerFacility/AbstractCompilerFacilityTest.kt
+++ analysis/analysis-api-impl-base/testFixtures/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/components/compilerFacility/AbstractCompilerFacilityTest.kt
@@ -23,6 +23,9 @@ import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.jvm.config.JvmClasspathRoot
 import org.jetbrains.kotlin.codegen.BytecodeListingTextCollectingVisitor
 import org.jetbrains.kotlin.codegen.forTestCompile.ForTestCompileRuntime
+import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+import org.jetbrains.kotlin.compiler.plugin.registerInProject
 import org.jetbrains.kotlin.config.CommonConfigurationKeys
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
@@ -85,6 +88,7 @@ abstract class AbstractCompilerFacilityTest : AbstractAnalysisApiBasedTest() {
         super.doTestByMainModuleAndOptionalMainFile(mainFile, mainModule, testServices)
     }
 
+    @OptIn(ExperimentalCompilerApi::class)
     override fun doTestByMainFile(mainFile: KtFile, mainModule: KtTestModule, testServices: TestServices) {
         val testFile = mainModule.testModule.files.single { it.name == mainFile.name }
 
@@ -92,8 +96,10 @@ abstract class AbstractCompilerFacilityTest : AbstractAnalysisApiBasedTest() {
         val irCollector = CollectingIrGenerationExtension(annotationToCheckCalls)
 
         val project = mainFile.project
-        project.extensionArea.getExtensionPoint(IrGenerationExtension.extensionPointName)
-            .registerExtension(irCollector, LoadingOrder.LAST, project)
+        with(CompilerPluginRegistrar.ExtensionStorage()) {
+            IrGenerationExtension.registerExtension(irCollector)
+            registerInProject(project)
+        }
 
         val compilerConfiguration = CompilerConfiguration().apply {
             put(CommonConfigurationKeys.MODULE_NAME, mainModule.testModule.name)
