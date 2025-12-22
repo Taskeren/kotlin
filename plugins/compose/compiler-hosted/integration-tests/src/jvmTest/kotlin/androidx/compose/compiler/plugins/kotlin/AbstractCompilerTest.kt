726347a19238389c26c3a2bf1fc556b9969ee082
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-12-22 14:57:15 +0200

    [Compiler] Unbind `IrGenerationExtension` from intellij extensions API
    Separate project extension point registration is created in AA modules
    for keeping the compatibility in the compiler facility components.
    
    ^KT-83341

--- plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/AbstractCompilerTest.kt
+++ plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/AbstractCompilerTest.kt
@@ -137,11 +137,8 @@ abstract class AbstractCompilerTest(val useFir: Boolean) {
                 with(ComposePluginRegistrar.Companion) {
                     registerCommonExtensions()
                 }
+                IrGenerationExtension.registerExtension(ComposePluginRegistrar.createComposeIrExtension(configuration))
             }
-            IrGenerationExtension.registerExtension(
-                this,
-                ComposePluginRegistrar.createComposeIrExtension(configuration)
-            )
         }
     )
 
