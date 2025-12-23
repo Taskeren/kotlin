d0ea6812406b49a189a3ee766fce85fa3223d9f5
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-12-23 11:57:29 +0200

    [CLI] Extract AllJavaSourcesInProjectScope from TopDownAnalyzerFacadeForJVM
    ^KT-83348

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/VfsBasedProjectEnvironment.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/VfsBasedProjectEnvironment.kt
@@ -187,7 +187,7 @@ open class VfsBasedProjectEnvironment(
         PsiBasedProjectFileSearchScope(ProjectScope.getLibrariesScope(project))
 
     override fun getSearchScopeForProjectJavaSources(): AbstractProjectFileSearchScope =
-        PsiBasedProjectFileSearchScope(TopDownAnalyzerFacadeForJVM.AllJavaSourcesInProjectScope(project))
+        PsiBasedProjectFileSearchScope(AllJavaSourcesInProjectScope(project))
 
     override fun getFirJavaFacade(
         firSession: FirSession,
