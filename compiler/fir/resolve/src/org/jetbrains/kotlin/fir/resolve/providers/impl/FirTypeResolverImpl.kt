4126e6a533967e9db07f6d61c38d9f90e234c5d2
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-01-15 19:01:51 +0200

    fixup! [FIR] Minor: Extract common logic to iterate over scopes to a function

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/providers/impl/FirTypeResolverImpl.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/providers/impl/FirTypeResolverImpl.kt
@@ -584,7 +584,6 @@ class FirTypeResolverImpl(private val session: FirSession) : FirTypeResolver() {
         }
     }
 
-
     private fun TypeResolutionConfiguration.iterateScopesWithSubstitution(
         name: Name,
         processor: (FirClassifierSymbol<*>, ConeSubstitutor, FirScope) -> Unit,
